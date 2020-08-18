5637d9c287b18c5028db3464c96fe64f80c82adf
BoBoBuild <3191241394@qq.com>
2020-08-18 21:09:29 +0800

    新剑法1

--- src/main/java/com/zhb/jianfa/dugujiujian/Newjianfa.java
+++ src/main/java/com/zhb/jianfa/dugujiujian/Newjianfa.java
@@ -3,5 +3,12 @@ package com.zhb.jianfa.dugujiujian;
 public class Newjianfa {
     public static void main(String[] args) {
         System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
+        System.out.println("新剑法！！");
     }
 }
