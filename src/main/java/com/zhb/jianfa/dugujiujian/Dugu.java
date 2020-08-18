c18ec834f00eeb102289c4dda3e42a674bfe08ed
CoCoBuild <1343350457@qq.com>
2020-08-18 20:32:12 +0800

    岳不群修改

--- src/main/java/com/zhb/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/zhb/jianfa/dugujiujian/Dugu.java
@@ -3,5 +3,10 @@ package com.zhb.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("剑法：独孤九剑！！！");
+        System.out.println("剑法：独孤九剑！！！");
+        System.out.println("剑法：独孤九剑！！！");
+        System.out.println("剑法：独孤九剑！！！");
+        System.out.println("剑法：独孤九剑！！！");
+        System.out.println("剑法：独孤九剑！！！");
     }
 }
